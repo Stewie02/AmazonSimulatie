527d7c66e5ba388d2f732b07c2492ffc491db892
Stewie02 <stefansijbesma12@gmail.com>
2020-11-07 10:33:13 +0100

    Update Constants.java

--- src/main/java/com/nhlstenden/amazonsimulatie/models/Constants.java
+++ src/main/java/com/nhlstenden/amazonsimulatie/models/Constants.java
@@ -9,9 +9,9 @@ public class Constants
     public static final double DISTANCE_BETWEEN_RACKS = 1.2;
     // This is the distance between the racks with a path between, from leftTop to leftTop
     public static final double DISTANCE_BETWEEN_RACK_WITH_PATH = 2.4;
-    // Amount of racks in the width
+    // Amount of racks in the width, it needs to be even!!!!
     public static final int AMOUNT_OF_RACKS_WIDTH = 28;
-    // Amount of racks in the height
+    // Amount of racks in the height, it needs to be even!!!!
     public static final int AMOUNT_OF_RACKS_HEIGHT = 16;
     // Size of an RackPosition
     public static final double RACK_POSITION_SIZE = 1;
